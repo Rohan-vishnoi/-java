79dbb8de395c5d1bd84d84e7b96778c7b45df0b6
rohan_vishnoi <avirohanvishnoi@gmail.com>
2018-06-19 17:38:11 +0530

    java complete

--- WebApplication6/src/java/com/mvc/util/DBConnection.java
+++ WebApplication6/src/java/com/mvc/util/DBConnection.java
@@ -2,11 +2,12 @@
 package com.mvc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 public class DBConnection {
 public static Connection createConnection()
 {
 Connection con = null;
-String url = "jdbc:mysql://localhost:3306/customers"; //MySQL URL followed by the database name
+String url = "jdbc:mysql://localhost:3306/customers?zeroDateTimeBehavior=convertToNull [root on Default schema]";
 String username = "root"; //MySQL username
 String password = "123456"; //MySQL password
 try 
@@ -22,7 +23,7 @@ e.printStackTrace();
 con = DriverManager.getConnection(url, username, password); //attempting to connect to MySQL database
 System.out.println("Printing connection object "+con);
 } 
-catch (Exception e) 
+catch (SQLException e) 
 {
 e.printStackTrace();
 }
